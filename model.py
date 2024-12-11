0e8a9553ec278313b67f9dcd1fd2bed1e30af8c9
Anas Lecaillon <anas@lecaillon.com>
2024-12-11 15:18:26 +0100

    added basics to tests and model

---
+++ model.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+@dataclass
+class GPT2Config:
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+
+class GPT2(nn.Module):
+    def __init__(self, config: GPT2Config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte=nn.Embedding(config.vocab_size, config.n_embd),
+            wpe=nn.Embedding(config.block_size, config.n_embd),
+            h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f=nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    @classmethod
+    def from_pretrained(cls):
+        from transformers import GPT2LMHeadModel
+        print(f"loading pretrained weights for model gpt2")
+        config = GPT2Config()
+        model = GPT2(config)
+        state_dict = model.state_dict()
+        keys = [key for key in state_dict.keys() if not key.endswith("attn.bias")]
+
+
+        model_hf = GPT2LMHeadModel.from_pretrained("gpt2")
+        state_dict_hf = model_hf.state_dict()
+        keys_hf = [key for key in state_dict_hf.keys() if not key.endswith("attn.bias") and not key.endswith("attn.masked_bias")]
+        transposed_keys_hf = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        for key in keys_hf:
+            if any(key.endswith(transposed_key) for transposed_key in transposed_keys_hf):
+                with torch.no_grad():
+                    state_dict[key].copy_(state_dict_hf[key].T)
+            else:
+                with torch.no_grad():
+                    state_dict[key].copy_(state_dict_hf[key])
+        return model
